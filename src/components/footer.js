63c307b28aa5ff218c1717a8944aa8aa48d580ae
Valerie Tuckler <valeriert2011@gmail.com>
2021-06-24 12:43:05 -0500

    Home page mobile and desktop version work properly and are completely stylized. Next is the 'Projects' page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,34 +1,39 @@
-import React from 'react';
-import behance from '../imgs/behance-50.png';
-import instagram from '../imgs/instagram-52.png';
-import mail from '../imgs/mail-50.png';
-import '../styles/footer.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import behance from "../imgs/behance-50.png";
+import instagram from "../imgs/instagram-52.png";
+import mail from "../imgs/mail-50.png";
+import "../styles/footer.css";
 
-function Footer(){
+function myFooter(){
     return(
-        <nav className="navbar myFooter navbar-expand-lg navbar-light bg-dark">
-            <div className="container-fluid">
-                <ul className="navbar-nav">
-                    <li className="navbar-item">
-                        <Link to ="#" className="navbar-brand">
-                            <img src={behance} alt="Behance logo" title="Behance logo" width="73%" height="40%"/>
-                        </Link>
-                    </li>      
-                    <li className="navbar-item">
-                        <Link to="#" className="navbar-brand">
-                            <img src={instagram} alt="Instagram logo" title="Instagram logo" width="68%" height="40%"/>
-                        </Link>
-                    </li>              
-                    <li className="navbar-item">
-                        <Link to="#" className="navbar-brand">
-                            <img src={mail} alt="Mail logo" title="Mail logo" width="75%" height="40%"/>
-                        </Link>
-                    </li>
-                </ul>                
+        <footer className="footer bg-dark fixed-bottom" id="myfooter" style={{backgroundColor:"grey"}}>
+            <div className="text-center container d-flex justify-content-center align-self-center py-3">
+                
+                <a href="https://www.behance.net/jordantuckler">
+                    {/*Button background color = dark*/}
+                    <img src={behance} alt="Behance logo" title = "Behance logo" className="btn btn-info btn-sm btn-floating bg-dark mx-3"/>                    
+                </a>    
+
+                <a href="https://www.instagram.com/jordantuckler/">
+                    {/*Button background color = dark*/}                    
+                    <img src={instagram} alt="Instagram logo"  title="Instagram logo"className="btn btn-info btn-sm btn-floating bg-dark mx-3" />                    
+                </a>                                            
+
+                <a href="mailto:jordantuckler@gmail.com">
+                    {/*Background color = dark*/}                    
+                    <img src={mail} alt="Mail logo" title="Mail logo" className="btn btn-info btn-sm btn-foating bg-dark mx-3"/>
+                </a>                                
             </div>
-        </nav>
+
+            {/* C O P Y R I G H T   L E T T E R S */}
+            
+            {/*Background color = dark*/}
+
+            <div className="text-center text-white p-1 bg-dark">
+                &copy; 2021 Jordan Tuckler
+            </div>
+        </footer>
     );
 };
 
-export default Footer;
+export default myFooter;
--- src/components/header.js
+++ src/components/header.js
@@ -4,37 +4,29 @@ import { Link } from 'react-router-dom';
 import "../styles/header.css";
 
 function Header(){
-    return(
-            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-                <div className="container-fluid">
-                    <span className="nav-text">
-                        <a href="/"className="navbar-brand">
-                            <img src={JLogo} alt="Architect Jordan's logo" width="100" height="56"/>
-                        </a>       
-                    </span>           
-                    <span className="nav-text">
-                        <a href="/projects" className="nav-link">
-                            Projects
-                        </a>
-                    </span>
-                    {/*
-                    <span>
-                        <Link to="/projects_info" className="nav-link">
-                            Projects Info
-                        </Link>
-                    </span>
-                    */}
-                    <span>
-                        <Link to="/about" className="nav-link">
-                            About
-                        </Link>                    
-                    </span>
-                    <span>
-                        <Link to="/contact" className="nav-link">
-                            Contact
-                        </Link>   
-                    </span>                                                                                         
-                </div>
+    return(        
+            <nav className="navbar navbar-expand-lg navbar-light bg-dark container-fluid">                          
+                        <span className="link-container">
+                            <Link to="/"className="navbar-brand">
+                                <img src={JLogo} alt="Architect Jordan's logo" title="&copy; Architect Jordan" width="100" height="56"/>
+                            </Link>                               
+                        </span>
+                        <span className="link-container">
+                            <Link href="/projects" className="nav-link" id="projects-link">
+                                Projects
+                            </Link>
+                        </span>
+                        <span className="link-container">
+                            <Link to="/about" className="nav-link" id="about-link">
+                                About
+                            </Link>                    
+                        </span>                        
+
+                        <span className="link-container">
+                            <Link to="/contact" className="nav-link" id="contact-link">
+                                Contact
+                            </Link>   
+                        </span>x                                                                                                                   
             </nav>        
     );
 };
