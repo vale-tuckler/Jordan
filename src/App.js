269259e113d9e1ea9c3cf3e07c118100c37de7e1
Valerie Tuckler <valeriert2011@gmail.com>
2021-07-04 21:20:28 -0500

    Sliders on 'Projects-Info' page have been properly sized and styled, however, the descriptions below every project still need styling. Next is the development of the 'Contact me' page.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <Route exact path = "/projects" component={Projects}/>
           <Route exact path = "/projects_info" component = {projectsInfo}/>
           <Route exact path = "/about" component = {About}/>
-          <Route exact path = "/contact" component = {Contact}/>      
+          <Route exact path = "/contact" component = {Contact}/>              
       </Switch>
     </BrowserRouter>
   );
--- src/pages/projects-info.js
+++ src/pages/projects-info.js
@@ -25,6 +25,8 @@ import containersApt4 from "../imgs/containers-apt/containers-apt4.png";
 import containersApt5 from "../imgs/containers-apt/containers-apt5.png";
 import containersApt6 from "../imgs/containers-apt/containers-apt6.png";
 
+import '../styles/projects-info.css';
+
 function ProjectsInfo(){
     return(
         <div>
@@ -32,131 +34,198 @@ function ProjectsInfo(){
                     
                     {/*MINIMALIST LOFT*/}
 
-                <Carousel fade id="minimalistLoft">
+                <Carousel id="minimalistLoft">
                     <Carousel.Item>
-                        <img 
-                            src={minimalistLoft1} 
-                            alt="Minimalist loft 1" 
-                            title="Minimalist loft picture 1"
-                            className="d-block w-75 h-100"
-                        />
+                       <div className="imgContainer">
+                            <img 
+                                src={minimalistLoft1} 
+                                alt="Minimalist loft 1" 
+                                title="Minimalist loft picture 1"
+                                className="d-block projectImg"
+                            />
+                       </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img 
-                            src={minimalistLoft2}
-                            alt="Minimalist loft 2"
-                            title="Minimalist loft picture 2"
-                            className="d-block w-75 h-100"
-                        />
+                        <div className="imgContainer">
+                            <img 
+                                src={minimalistLoft2}
+                                alt="Minimalist loft 2"
+                                title="Minimalist loft picture 2"
+                                className="d-block projectImg"
+                            />
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img 
-                            src={minimalistLoft3}
-                            alt="Minimalist loft 3"
-                            title="Minimalist loft picture 3"
-                            className="d-block w-75 h-100"
-                        />
+                        <div className="imgContainer">
+                            <img 
+                                src={minimalistLoft3}
+                                alt="Minimalist loft 3"
+                                title="Minimalist loft picture 3"
+                                className="d-block projectImg"
+                            />
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img 
-                            src={minimalistLoft4}
-                            alt="Minimalist loft 4"
-                            title="Minimalist loft picture 4"
-                            className="d-block w-75 h-100"
-                        />
+                        <div className="imgContainer">
+                            <img 
+                                src={minimalistLoft4}
+                                alt="Minimalist loft 4"
+                                title="Minimalist loft picture 4"
+                                className="d-block projectImg"
+                            />
+                        </div>
                     </Carousel.Item>
                 </Carousel>
+                <div className="projectDescription">
+                    <h2>What is Lorem Ipsum?</h2>
+                    <p className="d-flex justify-content">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type 
+                        specimen book. It has survived not only five centuries, but also the leap into 
+                        electronic typesetting, remaining essentially unchanged. It was popularised in 
+                        the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                        and more recently with desktop publishing software like Aldus PageMaker 
+                        including versions of Lorem Ipsum.
+                    </p>
+                </div>
 
                 {/* EFFICIENCY APARTMENT*/}
 
-                <Carousel fade id="efficiencyApt">
+                <Carousel id="efficiencyApt">
                     <Carousel.Item>
-                        <img 
-                            src={efficiencyApt1}
-                            alt="Efficiency apartment 1"
-                            title="Efficiency apartment picture 1"
-                            className="d-block w-75 h-100"
-                        />
+                        <div className="imgContainer">
+                            <img 
+                                src={efficiencyApt1}
+                                alt="Efficiency apartment 1"
+                                title="Efficiency apartment picture 1"
+                                className="d-block projectImg"
+                            />
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img 
-                            src={efficiencyApt2}
-                            alt="Efficiency apartment 2"
-                            title="Efficiency apartment picture 2"
-                            className="d-block w-75 h-100"
-                        />
+                        <div className="imgContainer">
+                            <img 
+                                src={efficiencyApt2}
+                                alt="Efficiency apartment 2"
+                                title="Efficiency apartment picture 2"
+                                className="d-block projectImg"
+                            />
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img 
-                            src={efficiencyApt3}
-                            alt="Efficiency apartment 3"
-                            title="Efficiency apartment picture 3"
-                            className="d-block w-75 h-100"
-                        />
+                        <div className="imgContainer">
+                            <img 
+                                src={efficiencyApt3}
+                                alt="Efficiency apartment 3"
+                                title="Efficiency apartment picture 3"
+                                className="d-block projectImg"
+                            />                        
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img 
-                            src={efficiencyApt4}
-                            alt="Efficiency apartment 4"
-                            title="Efficiency apartment picture 4"
-                            className="d-block w-75 h-100"
-                        />
+                        <div className="imgContainer">
+                            <img 
+                                src={efficiencyApt4}
+                                alt="Efficiency apartment 4"
+                                title="Efficiency apartment picture 4"
+                                className="d-block projectImg"
+                            />
+                        </div>
                     </Carousel.Item>
                 </Carousel>
+                <div className="projectDescription">
+                    <h2>What is Lorem Ipsum?</h2>
+                    <p className="d-flex justify-content">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type 
+                        specimen book. It has survived not only five centuries, but also the leap into 
+                        electronic typesetting, remaining essentially unchanged. It was popularised in 
+                        the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                        and more recently with desktop publishing software like Aldus PageMaker 
+                        including versions of Lorem Ipsum.
+                    </p>
+                </div>
 
                 {/* CONTAINERS APARTMENT */}
 
-                <Carousel fade id="containersApt">
+                <Carousel id="containersApt">
                     <Carousel.Item>
-                        <img 
-                            src={containersApt1}
-                            alt="Containers apartment 1"
-                            title="Containers apartment picture 1"
-                            className="d-block w-75 h-100"
-                        />
+                        <div className="imgContainer">
+                            <img 
+                                src={containersApt1}
+                                alt="Containers apartment 1"
+                                title="Containers apartment picture 1"
+                                className="d-block projectImg"
+                            />
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img 
-                            src={containersApt2}
-                            alt="Containers apartment 2"
-                            title="Containers apartment picture 2"
-                            className="d-block w-75 h-100"
-                        />
+                        <div className="imgContainer">
+                            <img 
+                                src={containersApt2}
+                                alt="Containers apartment 2"
+                                title="Containers apartment picture 2"
+                                className="d-block projectImg"
+                            />
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img 
-                            src={containersApt3}
-                            alt="Containers apartment 3"
-                            title="Containers apartment picture 3"
-                            className="d-block w-75 h-100"
-                        />
+                        <div className="imgContainer">
+                            <img 
+                                src={containersApt3}
+                                alt="Containers apartment 3"
+                                title="Containers apartment picture 3"
+                                className="d-block projectImg"
+                            />
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img 
-                            src={containersApt4}
-                            alt="Containers apartment 4"
-                            title="Containers apartment picture 4"
-                            className="d-block w-75 h-100"
-                        />
+                        <div className="imgContainer">
+                            <img 
+                                src={containersApt4}
+                                alt="Containers apartment 4"
+                                title="Containers apartment picture 4"
+                                className="d-block projectImg"
+                            />
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img 
-                            src={containersApt5}
-                            alt="Containers apartment 5"
-                            title="Containers apartment picture 5"
-                            className="d-block w-75 h-100"
-                        />
+                        <div className="imgContainer">
+                            <img 
+                                src={containersApt5}
+                                alt="Containers apartment 5"
+                                title="Containers apartment picture 5"
+                                className="d-block projectImg"
+                            />
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img 
-                            src={containersApt6}
-                            alt="Containers apartment 6"
-                            title="Containers apartment picture 6"
-                            className="d-block w-75 h-100"
-                        />
+                        <div className="imgContainer">
+                            <img 
+                                src={containersApt6}
+                                alt="Containers apartment 6"
+                                title="Containers apartment picture 6"
+                                className="d-block projectImg"
+                            />
+                        </div>
                     </Carousel.Item>
                 </Carousel>
-            <Footer/>
+                <div className="projectDescription" id="thirdDescription">
+                    <h2>What is Lorem Ipsum?</h2>
+                    <p className="d-flex justify-content">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type 
+                        specimen book. It has survived not only five centuries, but also the leap into 
+                        electronic typesetting, remaining essentially unchanged. It was popularised in 
+                        the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                        and more recently with desktop publishing software like Aldus PageMaker 
+                        including versions of Lorem Ipsum.
+                    </p>
+                </div>
+            <Footer/>            
         </div>
     ); 
 };
